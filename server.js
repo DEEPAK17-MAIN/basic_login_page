8f2146c49ed78ebeb087ebf03123dfd40b3f7525
Deepak <deepakkumarchandravanshi764@gmail.com>
2026-05-26 01:32:43 +0530

    Initial commit: Set up project structure, configuration, and user model

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const dotenv=require('dotenv');
+
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT||4000;
+
+
+
+
+
+
+app.listen(port,()=>{
+  console.log(`app is running on port:${port}`);
+  
+})
